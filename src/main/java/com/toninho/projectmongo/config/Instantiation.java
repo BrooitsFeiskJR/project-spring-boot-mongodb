5da7910ac632b0ca1c480558a241d306e5ec545e
BrooitsFeiskJR <luizantonio0125@gmail.com>
2023-03-06 20:25:19 -0300

    Database instantiation operation

---
+++ src/main/java/com/toninho/projectmongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.toninho.projectmongo.config;
+
+import com.toninho.projectmongo.domain.User;
+import com.toninho.projectmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User u1 = new User(null, "Rafaela Pavan", "rafaela@gmail.com");
+        User u2 = new User(null, "Luiz Antonio", "luiz@gmail.com");
+        User u3 = new User(null, "Matheus Rafanhin", "rafanhin@gmail.com");
+        User u4 = new User(null, "Igor menezes", "igor@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
+    }
+}
